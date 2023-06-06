7b0906ea4b066cfd4c3e07df3ea5576d32d18bb8
James van de Ven <hello@jamesvandeven.com>
2023-06-06 20:20:41 +0800

    added supabase client + auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { Header } from './components/Header'
 import TodoCreator from './components/TodoCreator'
 import TodoList from './components/TodoList'
+import { useAuthSession } from './hooks'
+import { supabase } from './supabase'
 
 function App() {
+  const session = useAuthSession()
   return (
     <div className="flex h-screen w-screen justify-center overflow-hidden bg-gray-900 text-gray-100">
-      <div className="my-4 flex w-full max-w-3xl flex-col gap-y-4 rounded-md p-12">
-        <Header />
-        <TodoCreator />
-        <TodoList />
-      </div>
+      {session ? (
+        <div className="my-4 flex w-full max-w-3xl flex-col gap-y-4 rounded-md p-12">
+          <Header />
+          <TodoCreator />
+          <TodoList />
+        </div>
+      ) : (
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      )}
     </div>
   )
 }
---
+++ src/hooks.tsx
@@ -0,0 +1,37 @@
+import { Session } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabase'
+import { Todo } from './types'
+
+export const useAuthSession = () => {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => setSession(session))
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) =>
+      setSession(session)
+    )
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return session
+}
+
+export const useFetchTodos = () => {
+  const [todos, setTodos] = useState<Todo[] | undefined>(undefined)
+
+  useEffect(() => {
+    supabase
+      .from('todos')
+      .select()
+      .then((data) => setTodos((data as unknown as Todo[]) ?? undefined))
+  }, [])
+
+  return todos
+}
