75cc04964ca36bbbc66b0b052caa52b0c560befe
James van de Ven <hello@jamesvandeven.com>
2023-06-10 14:47:53 +0800

    add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,61 @@
 import { Auth } from '@supabase/auth-ui-react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Header } from './components/Header'
 import TodoCreator from './components/TodoCreator'
 import TodoList from './components/TodoList'
 import { useAuthSession } from './hooks'
 import { supabase } from './supabase'
 
-function App() {
-  const session = useAuthSession()
-  return (
-    <div className="flex h-screen w-screen justify-center overflow-hidden bg-gray-900 text-gray-100">
-      {session ? (
-        <div className="my-4 flex w-full max-w-3xl flex-col gap-y-4 rounded-md p-12">
-          <Header />
-          <TodoCreator />
-          <TodoList />
-        </div>
-      ) : (
-        <div className="flex h-screen items-center">
-          <Auth
-            supabaseClient={supabase}
-            providers={['github']}
-            onlyThirdPartyProviders
-          />
-        </div>
-      )}
+const App = () => {
+  const { data: session, isError, isLoading } = useAuthSession()
+
+  if (isLoading) {
+    return <div className="flex h-screen items-center">loading...</div>
+  }
+
+  if (isError) {
+    return (
+      <div className="flex h-screen items-center">
+        Uh oh, encountered some error...
+      </div>
+    )
+  }
+
+  return session ? (
+    <div className="my-4 flex w-full max-w-3xl flex-col gap-y-4 rounded-md p-12">
+      <Header />
+      <TodoCreator />
+      <TodoList />
+    </div>
+  ) : (
+    <div className="flex h-screen items-center">
+      <Auth
+        supabaseClient={supabase}
+        providers={['github']}
+        onlyThirdPartyProviders
+      />
     </div>
   )
 }
 
-export default App
+const AppWithQueryContext = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false, // default: true
+      },
+    },
+  })
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="flex h-screen w-screen justify-center overflow-hidden bg-gray-900 text-gray-100">
+        <App />
+        <ReactQueryDevtools />
+      </div>
+    </QueryClientProvider>
+  )
+}
+
+export default AppWithQueryContext
